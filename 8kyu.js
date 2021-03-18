2668bec6f746e1ab75df48cf2a22694c9ebf485f
kent auyeung <kentayeung@gmail.com>
2021-03-18 09:03:18 -0700

    Added to 8kyu

--- 8kyu.js
+++ 8kyu.js
@@ -445,6 +445,16 @@ const grow = x => {
 
 // Beginner - Lost Without a Map 
 let maps = x => {
-  let newArr = x.map( num => num * 2)
-  return newArr
+  return x.map( num => num * 2)
+}
+
+// Transportation on vacation 
+function rentalCarCost(d) {
+  if(d < 3){
+    return d * 40
+  } else if(d < 7) {
+    return (d * 40) - 20
+  } else {
+    return (d * 40) - 50
+  }
 }
