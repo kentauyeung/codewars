260bc5072dff4c3b925f523781d48c69ee67bdd0
kent auyeung <kentayeung@gmail.com>
2021-03-20 19:18:24 -0700

    Added to 7kyu

--- 7kyu.js
+++ 7kyu.js
@@ -265,3 +265,15 @@ function planeSeat(a){
     nArr.sort()
     return nArr.indexOf(k) + 1
   }
+
+  // Ones and Zeros 
+  const binaryArrayToNumber = arr => {
+    arr.reverse()
+    let total = 0
+    for ( let i = 0; i < arr.length; i++ ) {
+      if( arr[i] === 1 ) {
+        total += Math.pow(2, i)
+      }
+    }
+    return total
+  }
