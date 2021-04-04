6815fb9cf94c8f833960e87dceadfa2374a6be44
kent auyeung <kentayeung@gmail.com>
2021-04-04 13:45:26 -0700

    Added to 7kyu

--- 7kyu.js
+++ 7kyu.js
@@ -634,3 +634,15 @@ function repeater(string, n){
   }
   return str
 }
+
+// Count the divisors of a number 
+function getDivisorsCnt(n){
+  let counter = 0
+  for (let i = 1; i <= n; i++ ) {
+    if ( n % i === 0 ) {
+      counter ++
+    }
+  }
+  
+  return counter
+}
