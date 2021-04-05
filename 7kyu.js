a7da438945fa84fb0ff65282b15df5b3a2efa30c
kent auyeung <kentayeung@gmail.com>
2021-04-05 10:56:26 -0700

    Added to 7kyu

--- 7kyu.js
+++ 7kyu.js
@@ -646,3 +646,13 @@ function getDivisorsCnt(n){
   
   return counter
 }
+
+// Credit Card Mask 
+function maskify(cc) {
+  const hidden = '#'
+  if ( cc.length > 4 ) {
+    return hidden.repeat(cc.length - 4) + cc.slice(-4)
+  } else {
+    return cc
+  }
+}
