217da82f29e8eeba835104e6092e0676c19a4d09
kent auyeung <kentayeung@gmail.com>
2021-04-09 10:09:19 -0700

    Added to 7kyu

--- 7kyu.js
+++ 7kyu.js
@@ -670,3 +670,9 @@ function broken(x){
                 '0' : '1'}
   return x.split('').map(x => pairs[x]).join('')
 }
+
+// Disemvowel Trolls 
+function disemvowel(str) {
+  let vowels = ['a', 'e', 'i', 'o', 'u', 'A', 'E', 'I', 'O', 'U']
+  return str.split('').filter( x => !vowels.includes(x)).join('')
+}
