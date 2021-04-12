5c989058f6087b74c899966198a0454a0d8568de
kent auyeung <kentayeung@gmail.com>
2021-04-12 09:10:35 -0700

    Added to 7kyu

--- 7kyu.js
+++ 7kyu.js
@@ -733,3 +733,12 @@ function well(x){
   
   return goodCount <= 2 && goodCount >= 1 ? 'Publish!' : goodCount >= 3 ? 'I smell a series!' : 'Fail!'
 }
+
+// All star code challenge #24 
+function hypotenuse(a, b){
+  return Math.sqrt( Math.pow(a, 2) + Math.pow(b, 2) )
+}
+
+function leg(c, a){
+  return Math.sqrt( Math.pow(c, 2) - Math.pow(a, 2) )
+}
