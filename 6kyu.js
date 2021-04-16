3aec05c8672377d7f3bc727e428a9a9c702cd1a6
kent auyeung <kentayeung@gmail.com>
2021-04-16 08:19:19 -0700

    Added to 6kyu

--- 6kyu.js
+++ 6kyu.js
@@ -172,3 +172,11 @@ function revrot(str, sz) {
   
   return newStr
 }
+
+// create phone number 
+function createPhoneNumber(numbers){
+  let areaCode = numbers.slice(0,3).join('')
+  let firstThree = numbers.slice(3,6).join('')
+  let lastFour = numbers.slice(6).join('')
+  return `(${areaCode}) ${firstThree}-${lastFour}`
+}
