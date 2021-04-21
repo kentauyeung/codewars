0a54b2af67214f73fa60c1bd3445cd52f0034b48
kent auyeung <kentayeung@gmail.com>
2021-04-21 16:59:29 -0700

    Attempted to refactor

--- 8kyu.js
+++ 8kyu.js
@@ -66,19 +66,7 @@ const langDB = {
   czech: 'Vitejte',
   danish: 'Velkomst',
   dutch: 'Welkom',
-  estonian: 'Tere tulemast',
-  finnish: 'Tervetuloa',
-  flemish: 'Welgekomen',
-  french: 'Bienvenue',
-  german: 'Willkommen',
-  irish: 'Failte',
-  italian: 'Benvenuto',
-  latvian: 'Gaidits',
-  lithuanian: 'Laukiamas',
-  polish: 'Witamy',
-  spanish: 'Bienvenido',
-  swedish: 'Valkommen',
-  welsh: 'Croeso'
+  estonian: 'Tere tulemast'
 }
 
 // Lario and Muigi Pipe Problem
