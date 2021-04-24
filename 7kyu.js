41a0b4dde32432e1fb175caa5a81e9e34721f470
Kent Auyeung <kentayeung@gmail.com>
2021-04-24 15:10:59 -0700

    Attempted to refactor

--- 7kyu.js
+++ 7kyu.js
@@ -583,7 +583,7 @@ function numOfOpenLockers(n){
 }
 
 // Heron's formula 
-function heron(a, b, c) {
+let heron = (a, b, c) => {
   let s = ( a + b + c) / 2
   return Math.sqrt(s * ( s - a ) * ( s - b ) * ( s - c ) ) 
 }
