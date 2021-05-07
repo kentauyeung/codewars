0c2a964ef6cea4e16e6d92929db68f164bd38d8d
kent auyeung <kentayeung@gmail.com>
2021-05-06 22:07:46 -0700

    Attempted to refactor

--- 8kyu.js
+++ 8kyu.js
@@ -411,7 +411,7 @@ let summation = num => {
 }
 
 // UEFA EURO 2016
-function uefaEuro2016(teams, scores){
+let uefaEuro2016 = (teams, scores) => {
   if(scores[0] > scores[1]){
     return `At match ${teams[0]} - ${teams[1]}, ${teams[0]} won!`
   } else if (scores[0] < scores[1]){
